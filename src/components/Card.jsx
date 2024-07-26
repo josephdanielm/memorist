6ba6b8a61a5fe0f6ec3b3efd5a03844b6b9c43fa
Joseph Mudd <josephmuddjr@gmail.com>
2024-07-25 22:40:32 -0500

    improvement: add keanu fallback

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function Card({ id, title, image_url, handleCardClick }) {
+  const [imgSrc, setImgSrc] = useState(image_url);
+
+  const handleError = () => {
+    setImgSrc("https://placekeanu.com/400");
+  };
+
   return (
     <button
       className="flex flex-col text-center justify-end"
@@ -7,9 +15,10 @@ export default function Card({ id, title, image_url, handleCardClick }) {
       onClick={handleCardClick}
     >
       <img
-        src={image_url}
+        src={imgSrc}
         className="h-auto max-w-full"
         referrerPolicy="no-referrer"
+        onError={handleError}
       />
       <h3 className="text-sm line-clamp-2 mx-auto">{title}</h3>
     </button>
